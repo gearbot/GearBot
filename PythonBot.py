c7c6f6f0749b761761ccfbfcf332ffcb340d5329
Slakscape <jbastenhof@live.nl>
2017-08-26 16:17:10 +0200

    Discord test code

---
+++ PythonBot.py
@@ -0,0 +1,27 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run('MzQ4MDMzMjY4OTk1NTg4MDk2.DIMTew.DJpmVS1j5kboblvziWa4HGZYKiI')
