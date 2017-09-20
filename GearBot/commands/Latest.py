9a486c40ad3d707923631ffa4ddba188b44833c2
AEnterprise <aenterprise2@gmail.com>
2017-09-20 09:48:50 +0200

    fix compat listing

--- GearBot/commands/Latest.py
+++ GearBot/commands/Latest.py
@@ -15,7 +15,7 @@ class Latest(Command):
         #     "content": "The latest version of BuildCraft is 7.99.7 and can be found at <https://mod-buildcraft.com/buildcraft-7997-alpha.html>",
         #     "embed": {
         #         "title": "The latest version of BuildCraft is 7.99.7",
-        #         "description": "",
+        #         "description": "Latest versions per MC version:",
         #         "url": "https://mod-buildcraft.com/buildcraft-7997-alpha.html",
         #         "color": 5559807,
         #         "timestamp": "2017-09-19T14:52:43.464Z",
@@ -46,16 +46,16 @@ class Latest(Command):
 
         embed = discord.Embed(title="The latest version of BuildCraft is 7.99.7", colour=discord.Colour(0x54d5ff),
                               url="https://mod-buildcraft.com/buildcraft-7997-alpha.html",
-                              description="Latest versions per MC version",
+                              description="Latest versions per MC version:",
                               timestamp=datetime.datetime.utcfromtimestamp(time.time()))
 
         embed.set_thumbnail(url="https://i.imgur.com/YKGkDDZ.png")
         embed.set_author(name="Buildcraft releases", url="http://www.mod-buildcraft.com/")
 
         embed.add_field(name="1.11.2",
-                        value="Buildcraft 7.99.7 (alpha)\n[Blog](https://mod-buildcraft.com/buildcraft-7997-alpha.html) | [Direct download](https://www.mod-buildcraft.com/releases/BuildCraft/7.99.7/buildcraft-7.99.7.jar)\n\nBuildcraft Compat 7.1.6\n[Blog](https://www.mod-buildcraft.com/buildcraft-7120-compat-716.html) | [Direct download](http://www.mod-buildcraft.com/releases/BuildCraftCompat/7.1.6/buildcraft-compat-7.1.6.jar)")
+                        value="Buildcraft 7.99.7 (alpha)\n[Blog](https://mod-buildcraft.com/buildcraft-7997-alpha.html) | [Direct download](https://www.mod-buildcraft.com/releases/BuildCraft/7.99.7/buildcraft-7.99.7.jar)")
         embed.add_field(name="1.7.10",
-                        value="Buildcraft 7.1.22 (stable)\n[Blog](https://www.mod-buildcraft.com/buildcraft-7994-alpha-7122-stable.html) | [Direct download](https://www.mod-buildcraft.com/releases/BuildCraft/7.1.22/buildcraft-7.1.22.jar)")
+                        value="Buildcraft 7.1.22 (stable)\n[Blog](https://www.mod-buildcraft.com/buildcraft-7994-alpha-7122-stable.html) | [Direct download](https://www.mod-buildcraft.com/releases/BuildCraft/7.1.22/buildcraft-7.1.22.jar)\n\nBuildcraft Compat 7.1.6\n[Blog](https://www.mod-buildcraft.com/buildcraft-7120-compat-716.html) | [Direct download](http://www.mod-buildcraft.com/releases/BuildCraftCompat/7.1.6/buildcraft-compat-7.1.6.jar)")
         embed.add_field(name="Older versions",
                         value="All other Buildcraft versions can be found in the [archives](https://www.mod-buildcraft.com/releases/)")
 
