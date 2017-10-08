e32260e9d162fe05c8e55845b9a3b5686866b482
AEnterprise <aenterprise2@gmail.com>
2017-10-08 19:08:14 +0200

    update test for test

--- GearBot/commands/Test.py
+++ GearBot/commands/Test.py
@@ -1,10 +1,12 @@
+import Variables
+from commands.OwnerCommands import OwnerCommand
 from commands.command import Command
 
 
-class Test(Command):
+class Test(OwnerCommand):
 
     def __init__(self):
         super().__init__("Testing things")
 
     async def execute(self, client, channel, user, params):
-        raise Exception("how does this work?")
+        raise Exception("just making sure this works")
