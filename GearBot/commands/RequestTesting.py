0a44be2ca66fff2895a15a1b2aa4bc07af579dec
AEnterprise <aenterprise2@gmail.com>
2017-11-20 15:53:06 +0100

    ping tester role

--- GearBot/commands/RequestTesting.py
+++ GearBot/commands/RequestTesting.py
@@ -13,8 +13,11 @@ class RequestTesting(RoleCommand):
         self.extraHelp["info"] = "Pings the tester group with a message"
 
     async def execute(self, client: discord.Client, channel: discord.Channel, user: discord.user.User, params) -> None:
+        role: discord.Role = discord.utils.get(channel.server.roles, id=configuration.getConfigVar("TESTER_ROLE_ID"))
+        await client.edit_role(channel.server, role, mentionable=True)
         embed = discord.Embed(title="New things to test!", description=" ".join(params[0::]))
         await client.send_message(Variables.ANNOUNCEMENTS_CHANNEL, f"<@&{configuration.getConfigVar('TESTER_ROLE_ID')}>", embed=embed)
+        await client.edit_role(channel.server, role, mentionable=False)
 
     def onReady(self, client: discord.Client):
         self.role = configuration.getConfigVar("DEV_ROLE_ID")
