68a14b001197632efd2c3ef36a6a12dd0eadec2d
AEnterprise <aenterprise2@gmail.com>
2017-11-20 19:49:14 +0100

    quote improvements

--- GearBot/commands/Quote.py
+++ GearBot/commands/Quote.py
@@ -16,17 +16,28 @@ class Quote(Command):
             await client.send_message(channel, "Invalid param")
             return
 
-        for ch in channel.server.channels:
-            try:
-                message:discord.Message = await client.get_message(ch, params[0])
-                embed = discord.Embed(colour=discord.Colour(0xd5fff), description=message.content,
-                                      timestamp=message.timestamp)
-                embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
-                embed.set_footer(text=f"Quote requested by {user.name}")
-                await client.send_message(channel, embed=embed)
-                return
-
-            except Exception as e:
-                pass
-
-        await client.send_message(channel, "Unable to find that message")
+        await client.send_typing(channel)
+        found = False
+        message = False
+        try:
+            message = await client.get_message(channel, params[0])
+            found = True
+        except Exception as e:
+            pass
+        if not found:
+            for ch in channel.server.channels:
+                try:
+                    message:discord.Message = await client.get_message(ch, params[0])
+                    break
+                except Exception as e:
+                    pass
+        if not message == False:
+            embed = discord.Embed(colour=discord.Colour(0xd5fff), description=message.content,
+                                  timestamp=message.timestamp)
+            embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+            if len(message.attachments) == 1:
+                embed.set_image(url=message.attachments[0]["url"])
+            embed.set_footer(text=f"Quote requested by {user.name}")
+            await client.send_message(channel, embed=embed)
+        else:
+            await client.send_message(channel, "Unable to find that message")
