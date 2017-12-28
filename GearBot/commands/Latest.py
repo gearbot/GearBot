944ad0188d42f948154006d51078c3a7fe10f2dd
AEnterprise <aenterprise2@gmail.com>
2017-12-28 09:41:36 +0100

    more work on version fetching

--- GearBot/commands/Latest.py
+++ GearBot/commands/Latest.py
@@ -15,38 +15,7 @@ class Latest(Command):
         self.extraHelp["info"] = "Displays the latest versions for the latest MC versions and their blog/download links"
 
     async def execute(self, client, channel, user, params):
-        # {
-        #     "content": "The latest version of BuildCraft is 7.99.7 and can be found at <https://mod-buildcraft.com/buildcraft-7997-alpha.html>",
-        #     "embed": {
-        #         "title": "The latest version of BuildCraft is 7.99.7",
-        #         "description": "Latest versions per MC version:",
-        #         "url": "https://mod-buildcraft.com/buildcraft-7997-alpha.html",
-        #         "color": 5559807,
-        #         "timestamp": "2017-09-19T14:52:43.464Z",
-        #
-        #         "thumbnail": {
-        #             "url": "https://i.imgur.com/YKGkDDZ.png"
-        #         },
-        #         "author": {
-        #             "name": "Buildcraft releases",
-        #             "url": "http://www.mod-buildcraft.com/"
-        #         },
-        #         "fields": [
-        #             {
-        #                 "name": "1.11.2",
-        #                 "value": "Buildcraft 7.99.7 (alpha)\n[Blog](https://mod-buildcraft.com/buildcraft-7997-alpha.html) | [Direct download](https://www.mod-buildcraft.com/releases/BuildCraft/7.99.7/buildcraft-7.99.7.jar)"
-        #             },
-        #             {
-        #                 "name": "1.7.10",
-        #                 "value": "Buildcraft 7.1.22 (stable)\n[Blog](https://www.mod-buildcraft.com/buildcraft-7994-alpha-7122-stable.html) | [Direct download](https://www.mod-buildcraft.com/releases/BuildCraft/7.1.22/buildcraft-7.1.22.jar)\n\nBuildcraft Compat 7.1.6\n[Blog](https://www.mod-buildcraft.com/buildcraft-7120-compat-716.html) | [Direct download](http://www.mod-buildcraft.com/releases/BuildCraftCompat/7.1.6/buildcraft-compat-7.1.6.jar)"
-        #             },
-        #             {
-        #                 "name": "Older versions",
-        #                 "value": "All other Buildcraft versions can be found in the [archives](https://www.mod-buildcraft.com/releases/)"
-        #             }
-        #         ]
-        #     }
-        # }
+
 
         latest = VersionInfo.versions[VersionInfo.getSortedVersions()[0]]
         embed = discord.Embed(title=f"The latest version of BuildCraft is {latest['BC_VERSION']}", colour=discord.Colour(0x54d5ff),
--- GearBot/versions/VersionChecker.py
+++ GearBot/versions/VersionChecker.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import urllib.request
+import json
 
 from Util import configuration
 
@@ -11,33 +12,45 @@ BCT_VERSION_LIST = {}
 async def runVersionChecker():
     while True:#not client.is_closed:
         logging.info("Version check initiated")
-        with urllib.request.urlopen("https://www.mod-buildcraft.com/build_info_full/last_change.txt") as request:
-            #await asyncio.sleep(1)  # sleep to have the bot loop continue from time to time, the url request might take time when connection is slow
-            prevChange = configuration.getConfigVar("BC_INFO_LAST_CHANGE", 0)
-            timestamp = int(request.read())
-            logging.info(timestamp)
-            if timestamp > prevChange or True:
-                logging.info("Timestamp updated, on to finding the new things")
-                configuration.setConfigVar("BC_INFO_LAST_CHANGE", timestamp)
-                #await asyncio.sleep(1)
-                logging.info("Fetching BuildCraft releases")
-                with urllib.request.urlopen("https://www.mod-buildcraft.com/build_info_full/BuildCraft/versions.txt") as bcRequest:
-                    content = bcRequest.read()
-                    versionlistBC = str(content)[2:].split(r"\n")
-                    for version in versionlistBC:
-                        if not version in BC_VERSION_LIST.keys():
-                            pass
-                    logging.info(f"found {len(versionlistBC)} BuildCraft releases")
-                with urllib.request.urlopen("https://www.mod-buildcraft.com/build_info_full/BuildCraftCompat/versions.txt") as bccRequest:
-                    versionlistBCC = str(bccRequest.read())[2:].split(r"\n")
-                    logging.info(f"found {len(versionlistBCC)} BuildCraft Compat releases")
-                with urllib.request.urlopen("https://www.mod-buildcraft.com/build_info_full/testing/BuildCraft/versions.txt") as bctRequest:
-                    versionlistBCT = str(bctRequest.read())[2:].split(r"\n")
-                    logging.info(f"found {len(versionlistBCT)} BuildCraft pre-releases")
+        timestamp = int(await getFileContent("https://www.mod-buildcraft.com/build_info_full/last_change.txt"))
+        prevChange = configuration.getConfigVar("BC_INFO_LAST_CHANGE", 0)
+        logging.info(timestamp)
+        if timestamp > prevChange or True:
+            logging.info("Timestamp updated, on to finding the new things")
+            configuration.setConfigVar("BC_INFO_LAST_CHANGE", timestamp)
+            logging.info("Fetching BuildCraft releases")
+            content = await getFileContent("https://www.mod-buildcraft.com/build_info_full/BuildCraft/versions.txt")
+            versionlistBC = content.decode("utf-8").split("\n")
+            logging.info(f"found {len(versionlistBC)} BuildCraft releases")
+            for version in versionlistBC:
+                if not version == "" and not version in BC_VERSION_LIST.keys():
+                    try:
+                        with urllib.request.urlopen(f"https://www.mod-buildcraft.com/build_info_full/BuildCraft/{version}.json") as inforequest:
+                            info = json.load(inforequest)
+                            if not "mc_version" in info.keys():
+                                # no info, set to dummy
+                                info["mc_version"] = "Unknown"
+                            # await asyncio.sleep(1)
+                        BC_VERSION_LIST[version] = info
+                    except Exception as ex:
+                        logging.error(f"Failed to fetch info for {version}")
+
+            content = await getFileContent("https://www.mod-buildcraft.com/build_info_full/BuildCraftCompat/versions.txt")
+            versionlistBCC = content.decode("utf-8").split("\n")
+            logging.info(f"found {len(versionlistBCC)} BuildCraft Compat releases")
+            content = await getFileContent("https://www.mod-buildcraft.com/build_info_full/testing/BuildCraft/versions.txt")
+            versionlistBCT = content.decode("utf-8").split("\n")
+            logging.info(f"found {len(versionlistBCT)} BuildCraft pre-releases")
 
 
 
         await asyncio.sleep(8)
+
+async def getFileContent(url):
+    with urllib.request.urlopen(url) as request:
+        content = request.read()
+    #await asyncio.sleep(1)
+    return content
 
 from logging import INFO
 logging.basicConfig(level=INFO, format="%(asctime)s %(levelname)s %(message)s")
