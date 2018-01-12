673424d827281fa686e8cb2b8d164c787a2cf83c
AEnterprise <aenterprise2@gmail.com>
2018-01-12 20:55:56 +0100

    catch info without params

--- GearBot/commands/info.py
+++ GearBot/commands/info.py
@@ -4,7 +4,7 @@ import time
 import discord
 
 from commands.command import Command
-from versions import VersionChecker
+from versions import VersionChecker, VersionInfo
 
 
 class Info(Command):
@@ -13,8 +13,12 @@ class Info(Command):
     def __init__(self) -> None:
         super().__init__()
         self.extraHelp["info"] = "Gives info about releases"
+        self.extraHelp["example usage"] = f"!info {VersionInfo.getLatest(VersionChecker.BC_VERSION_LIST)}"
 
     async def execute(self, client: discord.Client, channel: discord.Channel, user: discord.user.User, params) -> None:
+        if len(params) == 0:
+            await client.send_message(channel, "I'm sorry i'm an expert on gears and BuildCraft releases, not a creepy mind reading bot (yet), please try telling me what version you want more info about")
+            return
         v = params[0]
         if not (v in VersionChecker.BC_VERSION_LIST.keys() or v in VersionChecker.BCC_VERSION_LIST.keys() or v in VersionChecker.BCT_VERSION_LIST.keys()):
             await client.send_message(channel, "I'm sorry but even with all my gears i can't seem to find that version, are you sure it exists?")
