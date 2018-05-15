8a476e002a85a85a9b4e75355513fd8288b26c48
AEnterprise <aenterprise2@gmail.com>
2018-05-15 16:41:02 +0200

    move serverinfo command

--- GearBot/Cogs/Admin.py
+++ GearBot/Cogs/Admin.py
@@ -82,38 +82,6 @@ class Admin:
             await Configuration.onReady(self.bot)
             await ctx.send("Configs reloaded")
 
-    @commands.command()
-    async def serverinfo(self, ctx):
-        """Shows information about the current server."""
-        guild_features = ", ".join(ctx.guild.features)
-        print(guild_features)
-        if guild_features == "":
-            guild_features = None
-        role_list = []
-        for i in range(len(ctx.guild.roles)):
-            role_list.append(ctx.guild.roles[i].name)
-        guild_made = ctx.guild.created_at.strftime("%d-%m-%Y")
-        embed = discord.Embed(color=0x7289DA)
-        embed.set_thumbnail(url=ctx.guild.icon_url)
-        requested_at = ctx.message.created_at.strftime("%d-%m-%Y %I:%M%p")
-        embed.set_footer(text=f"Requested by {ctx.author.name} at {requested_at}", icon_url=ctx.author.avatar_url)
-        embed.add_field(name="Name", value=ctx.guild.name, inline=True)
-        embed.add_field(name="ID", value=ctx.guild.id, inline=True)
-        embed.add_field(name="Owner", value=ctx.guild.owner, inline=True)
-        embed.add_field(name="Members", value=ctx.guild.member_count, inline=True)
-        embed.add_field(name="Text Channels", value=len(ctx.guild.text_channels), inline=True)
-        embed.add_field(name="Voice Channels", value=len(ctx.guild.voice_channels), inline=True)
-        embed.add_field(name="Total Channels", value=len(ctx.guild.text_channels) + len(ctx.guild.voice_channels),
-                        inline=True)
-        embed.add_field(name="Created at",
-                        value=f"{guild_made} ({(ctx.message.created_at - ctx.guild.created_at).days} days ago)",
-                        inline=True)
-        embed.add_field(name="VIP Features", value=guild_features, inline=True)
-        if ctx.guild.icon_url != "":
-            embed.add_field(name="Server Icon URL", value=ctx.guild.icon_url, inline=True)
-        embed.add_field(name="Roles", value=", ".join(role_list), inline=True)
-        await ctx.send(embed=embed)
-
 
 def setup(bot):
     bot.add_cog(Admin(bot))
