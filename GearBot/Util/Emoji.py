c70ed1ee5a72e9e20269adb8d81ec4357f47d3eb
AEnterprise <aenterprise2@gmail.com>
2018-08-27 13:22:31 +0200

    add fall back emoji

--- GearBot/Util/Emoji.py
+++ GearBot/Util/Emoji.py
@@ -4,14 +4,41 @@ from Util import Configuration
 
 emojis = dict()
 
+BACKUPS= {
+        "INNOCENT": "😇",
+        "JOIN": "📥",
+        "LEAVE": "📤",
+        "LEFT": "◀",
+        "LOADING": "⏳",
+        "MUTE": "😶",
+        "NAMETAG": "📛",
+        "NICKTAG": "📛",
+        "NO": "🚫",
+        "REFRESH": "🔁",
+        "RIGHT": "▶",
+        "WARNING": "⚠",
+        "WHAT": "☹",
+        "YES": "✅",
+        "DIAMOND": "⚙",
+        "GOLD": "⚙",
+        "IRON": "⚙",
+        "STONE": "⚙",
+        "WOOD": "⚙",
+        "TODO": "📋"
+    }
 def on_ready(bot):
     for name, eid in Configuration.getMasterConfigVar("EMOJI").items():
         emojis[name] = utils.get(bot.emojis, id=eid)
 
 def get_chat_emoji(name):
-    emoji = emojis[name]
-    return f"<:{emoji.name}:{emoji.id}>"
+    if name in emojis:
+        emoji = emojis[name]
+        return f"<:{emoji.name}:{emoji.id}>"
+    else:
+        return BACKUPS[name]
 
 def get_emoji(name):
-    #TODO: fall back to unicode if not found
-    return emojis[name]
+    if name in emojis:
+        return emojis[name]
+    else:
+        return BACKUPS[name]
