e70f64a6f961c8d5c27d80fb8a3c8d978795dfbb
AEnterprise <aenterprise2@gmail.com>
2018-08-28 10:32:13 +0200

    clean up and account for other bots being faster

--- GearBot/Cogs/Censor.py
+++ GearBot/Cogs/Censor.py
@@ -7,6 +7,19 @@ from discord.ext.commands import clean_content
 from Util import Configuration, GearbotLogging, Permissioncheckers, Translator, Utils
 
 INVITE_MATCHER = re.compile(r"(?:https?:\/\/)?(?:www\.)?(?:discord\.(?:gg|io|me|li)|discordapp\.com\/invite)\/([\w|\d|-]+)", flags=re.IGNORECASE)
+
+
+async def censor(ctx, code, server_name):
+    try:
+        await ctx.message.delete()
+        clean_message = await clean_content().convert(ctx, ctx.message.content)
+        clean_name = Utils.clean_user(ctx.message.author)
+        await GearbotLogging.log_to_minor_log(ctx.message.guild,
+                                              f":no_entry_sign: {Translator.translate('censored_invite', ctx.guild.id, user=clean_name, code=code, message=clean_message, server_name=server_name)}")
+    except discord.Forbidden:
+        pass # we failed? guess we lost the race
+
+
 class Censor:
 
     def __init__(self, bot):
@@ -29,17 +42,11 @@ class Censor:
                 except discord.NotFound:
                     pass
                 except KeyError:
-                    await message.delete()
-                    clean_message = await clean_content().convert(ctx, message.content)
-                    clean_name = Utils.clean_user(message.author)
-                    await GearbotLogging.log_to_minor_log(message.guild,
-                                                          f":no_entry_sign: {Translator.translate('censored_invite', ctx.guild.id, user=clean_name, code=code, message=clean_message, server_name='DM group')}")
+                    await censor(ctx, code, "DM group")
                 else:
                     if invite.guild is None or (not invite.guild.id in guilds and invite.guild.id != guild.id):
-                        await message.delete()
-                        clean_message = await clean_content().convert(ctx ,message.content)
-                        clean_name = Utils.clean_user(message.author)
-                        await GearbotLogging.log_to_minor_log(message.guild, f":no_entry_sign: {Translator.translate('censored_invite', ctx.guild.id, user=clean_name, code=code, message=clean_message, server_name=invite.guild.name)}")
+                        await censor(ctx, code, invite.guild.name)
+
 
 def setup(bot):
     bot.add_cog(Censor(bot))
