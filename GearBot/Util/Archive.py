cc8c6ec4b33d366b6a2ec6b828906cf10960f493
AEnterprise <aenterprise2@gmail.com>
2018-08-28 13:13:54 +0200

    don't leek during archive races

--- GearBot/Util/Archive.py
+++ GearBot/Util/Archive.py
@@ -6,15 +6,19 @@ import discord
 from Util import Utils, GearbotLogging, Translator, Emoji
 from database.DatabaseConnector import LoggedAttachment
 
+archive_counter = 0
 
 async def archive_purge(bot, guild_id, messages):
+    global archive_counter
+    archive_counter += 1
     channel = bot.get_channel(list(messages.values())[0].channel)
     out = f"purged at {datetime.datetime.now()} from {channel.name}\n"
     out += await pack_messages(messages.values())
-    filename = "Purged messages archive.txt"
+    filename = f"Purged messages archive {archive_counter}.txt"
     with open(filename, "w", encoding="utf-8") as file:
         file.write(out)
-    await GearbotLogging.log_to_minor_log(bot.get_guild(guild_id), message=Translator.translate('purged_log', guild_id, count=len(messages), channel=channel.mention), file=discord.File(filename))
+    with open (filename, "rb") as file:
+        await GearbotLogging.log_to_minor_log(bot.get_guild(guild_id), message=Translator.translate('purged_log', guild_id, count=len(messages), channel=channel.mention), file=discord.File(file, "Purged messages archive.txt"))
     os.remove(filename)
 
 async def pack_messages(messages):
@@ -24,8 +28,11 @@ async def pack_messages(messages):
         out += f"{datetime.datetime.fromtimestamp(message.timestamp)} {message.server} - {message.channel} - {message.messageid} | {name} ({message.author}) | {message.content} | {', '.join(attachment.url for attachment in LoggedAttachment.select().where(LoggedAttachment.messageid == message.messageid))}\n"
     return out
 
-async def ship_messages(ctx, messages, filename="Message archive.txt"):
+async def ship_messages(ctx, messages, filename="Message archive"):
     if len(messages) > 0:
+        global archive_counter
+        archive_counter += 1
+        real_name = f"{filename} {archive_counter}.txt"
         message_list = dict()
         for message in messages:
             message_list[message.messageid] = message
@@ -33,9 +40,10 @@ async def ship_messages(ctx, messages, filename="Message archive.txt"):
         for mid, message in sorted(message_list.items()):
             messages.append(message)
         out = await pack_messages(messages)
-        with open(filename, "w", encoding="utf-8") as file:
+        with open(real_name, "w", encoding="utf-8") as file:
             file.write(out)
-        await ctx.send(f"{Emoji.get_chat_emoji('YES')} {Translator.translate('archived_count', ctx, count=len(messages))}", file=discord.File(filename))
-        os.remove(filename)
+        with open(real_name, "rb") as file:
+            await ctx.send(f"{Emoji.get_chat_emoji('YES')} {Translator.translate('archived_count', ctx, count=len(messages))}", file=discord.File(file, f"{filename}.txt"))
+        os.remove(real_name)
     else:
         await ctx.send(f"{Emoji.get_chat_emoji('WARNING')} {Translator.translate('archive_empty', ctx)}")
