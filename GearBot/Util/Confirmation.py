53c75105cc324233e7aed2851234184844380f62
AEnterprise <aenterprise2@gmail.com>
2018-11-05 14:50:06 +0100

    track confirmations to see what might be breaking them

--- GearBot/Util/Confirmation.py
+++ GearBot/Util/Confirmation.py
@@ -11,21 +11,32 @@ noID = 465582003874693130
 yes = None
 no = None
 
+
 def on_ready(bot):
     global yes, no
     yes = utils.get(bot.emojis, id=yesID)
     no = utils.get(bot.emojis, id=noID)
 
-async def confirm(ctx:commands.Context, text, timeout=30, on_yes = None, on_no = None, delete=True):
-    message:discord.Message = await ctx.send(text)
+
+async def confirm(ctx: commands.Context, text, timeout=30, on_yes=None, on_no=None, delete=True):
+    bot = ctx.bot
+    message: discord.Message = await ctx.send(text)
     await message.add_reaction(yes)
     await message.add_reaction(no)
 
-    def check(reaction:discord.Reaction, user):
+    def check(reaction: discord.Reaction, user):
         return user == ctx.message.author and reaction.emoji in (yes, no) and reaction.message.id == message.id
 
     try:
-        reaction, user = await ctx.bot.wait_for('reaction_add', timeout=timeout, check=check)
+        task = ctx.bot.loop.create_task(ctx.bot.wait_for('reaction_add', timeout=timeout, check=check))
+        if not hasattr(bot, 'wait_for_debug_tasks'):
+            bot.wait_for_debug_tasks = dict()
+        bot.wait_for_debug_tasks[message.id] = {
+            task: task,
+            message: message,
+            ctx: ctx
+        }
+        reaction, user = await task
     except asyncio.TimeoutError:
         await message.delete()
         await ctx.send(f"I got no answer within {timeout} seconds.. Aborting.")
@@ -47,3 +58,4 @@ async def confirm(ctx:commands.Context, text, timeout=30, on_yes = None, on_no =
                 await on_no()
             else:
                 await GearbotLogging.send_to(ctx, "NO", "command_canceled")
+    del bot.wait_for_debug_tasks[message.id]
