db1d2763b2bd9b0dcb8b130cc6d0a9d0a4a76f7f
AEnterprise <aenterprise2@gmail.com>
2018-12-10 00:07:25 +0100

    Update ModLog.py

--- GearBot/Cogs/ModLog.py
+++ GearBot/Cogs/ModLog.py
@@ -441,7 +441,7 @@ class ModLog:
                         new_value = getattr(a_override, perm)
                         if value != new_value:
                             parts = dict(before=self.prep_override(value), after=self.prep_override(new_value),
-                                         permission=perm, channel=after, target_name=Utils.clean(target),
+                                         permission=perm, channel=after, target_name=await Utils.clean(target),
                                          target_id=target.id)
                             key = "permission_override_update"
 
