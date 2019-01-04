441686361cbe0ae9c26491ee680fb333ed818370
AEnterprise <aenterprise2@gmail.com>
2019-01-04 11:10:38 +0100

    increase beaning

--- GearBot/Cogs/Moderation.py
+++ GearBot/Cogs/Moderation.py
@@ -232,7 +232,7 @@ class Moderation:
         if allowed:
             await GearbotLogging.send_to(ctx, "YES", "bean_confirmation", user=Utils.clean_user(user), user_id=user.id, reason=reason)
             try :
-                message = await self.bot.wait_for("message", timeout=60, check=lambda m: m.author == user)
+                message = await self.bot.wait_for("message", timeout=60*5, check=lambda m: m.author == user)
             except asyncio.TimeoutError:
                 pass
             else:
