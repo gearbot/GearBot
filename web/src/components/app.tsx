19c86b9213acb2f501f4f43c0749a7adf5c09fb8
AEnterprise <aenterprise2@gmail.com>
2019-01-31 19:45:00 +0100

    doc tweaks

--- web/src/components/app.tsx
+++ web/src/components/app.tsx
@@ -1,5 +1,5 @@
-import {Component, h} from "preact";
-import {Router} from "preact-router";
+import {Component} from "preact";
+import {route, Router} from "preact-router";
 
 import Header from "./header";
 // Code-splitting is automated for routes
@@ -11,6 +11,27 @@ import {DashboardState} from "./state";
 import Gear from "./gear";
 
 export default class App extends Component<{}, DashboardState> {
+
+	componentDidMount(): void {
+		addEventListener("click", this.cheat);
+		addEventListener("touch", this.cheat);
+	}
+
+	cheat(e) {
+		var a = e.target;
+		if (a.tagName == "A") {
+			let target = a.getAttribute("href");
+			if (target.startsWith("/docs")) {
+				route(target);
+				e.preventDefault();
+				e.stopImmediatePropagation();
+				e.stopPropagation();
+				return false;
+			}
+		}
+	}
+
+
 	handleRoute = (e: { url: string; }) => {
 		this.setState({
 			currentUrl: e.url
@@ -20,17 +41,18 @@ export default class App extends Component<{}, DashboardState> {
 	render() {
 		return (
 			<div id="app">
-				<Header image="https://cdn.discordapp.com/avatars/106354106196570112/097e0f5e83f747e5ae684f9180eb6dba.png?size=128"/>
-					<Router onChange={this.handleRoute}>
-						<Home path="/" />
-						<Dashboard path="/dashboard"/>
-						<Docs path="/docs/" doc="index"/>
-						<Docs path="/docs/:folder?/:doc?"/>
-					</Router>
+				<Header
+					image="https://cdn.discordapp.com/avatars/106354106196570112/097e0f5e83f747e5ae684f9180eb6dba.png?size=128"/>
+				<Router onChange={this.handleRoute}>
+					<Home path="/"/>
+					<Dashboard path="/dashboard"/>
+					<Docs path="/docs/" doc="index"/>
+					<Docs path="/docs/:folder?/:doc?"/>
+				</Router>
 				<div class="gearFooter">
 					<Gear size={500}/>
 				</div>
-				</div>
+			</div>
 		);
 	}
 }
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,6 @@
-import {h, render} from "preact";
+import {render} from "preact";
 // Stylesheets
 import "./style/styles.scss";
 import App from "./components/app";
 
-render((<App></App>), document.body);
+render((<App/>), document.body);
