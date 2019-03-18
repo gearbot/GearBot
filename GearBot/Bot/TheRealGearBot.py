5ad146e0fce9c0cb9df1e3319d0513f68c180df3
AEnterprise <aenterprise2@gmail.com>
2019-03-18 14:42:50 +0100

    silly tarek

--- GearBot/Bot/TheRealGearBot.py
+++ GearBot/Bot/TheRealGearBot.py
@@ -156,7 +156,7 @@ async def on_message(bot, message:Message):
 async def on_guild_join(guild):
     GearbotLogging.info(f"A new guild came up: {guild.name} ({guild.id}).")
     Configuration.load_config(guild.id)
-    await GearbotLogging.bot_log(f"{Emoji.get_chat_emoji('JOIN')} A new guild came up: {guild.name} ({guild.id}).", embed=Utils.server_info(guild))
+    await GearbotLogging.bot_log(f"{Emoji.get_chat_emoji('JOIN')} A new guild came up: {Utils.clean(guild.name)} ({guild.id}).", embed=Utils.server_info(guild))
 
 async def on_guild_remove(guild):
     GearbotLogging.info(f"I was removed from a guild: {guild.name} ({guild.id}).")
