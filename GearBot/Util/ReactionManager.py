19b21b0c097fa6395d6ea9c5bddc6c9624e98cdc
AEnterprise <aenterprise2@gmail.com>
2019-03-30 21:12:41 +0100

    fix some weird cases on reactions

--- GearBot/Util/ReactionManager.py
+++ GearBot/Util/ReactionManager.py
@@ -7,6 +7,9 @@ from Util import Emoji, Pages, InfractionUtils, Selfroles, Translator, Configura
 
 async def paged(bot, message, user_id, reaction, **kwargs):
     user = message.channel.guild.get_member(user_id)
+    if user is None:
+        await remove_reaction(message, reaction, await bot.fetch_user(user_id))
+        return
     left = Emoji.get_chat_emoji('LEFT')
     right = Emoji.get_chat_emoji('RIGHT')
     refresh = Emoji.get_chat_emoji('REFRESH')
@@ -24,16 +27,16 @@ async def paged(bot, message, user_id, reaction, **kwargs):
 
 async def self_roles(bot, message, user_id, reaction, **kwargs):
     user = message.channel.guild.get_member(user_id)
-    if user is not None:
-        bot.loop.create_task(remove_reaction(message, reaction, user))
+    if user is None:
+        await remove_reaction(message, reaction, await bot.fetch_user(user_id))
+        return
+    bot.loop.create_task(remove_reaction(message, reaction, user))
     left = Emoji.get_chat_emoji('LEFT')
     right = Emoji.get_chat_emoji('RIGHT')
     refresh = Emoji.get_chat_emoji('REFRESH')
     r2 = "🔁"
     page_num = int(kwargs.get("page_num", 0))
     add = reaction not in [left, right, refresh, r2]
-    if not add:
-        bot.loop.create_task(remove_reaction(message, reaction, user))
     if str(reaction) == left:
         page_num -= 1
         add = False
@@ -42,27 +45,27 @@ async def self_roles(bot, message, user_id, reaction, **kwargs):
         add = False
     elif str(reaction) in [refresh, r2]:
         add = False
-    if user is not None:
-        if add:
-            for i in range(10):
-                if str(reaction) == str(Emoji.get_emoji(str(i+1))):
-                    roles = Configuration.get_var(message.guild.id, "SELF_ROLES")
-                    role = message.channel.guild.get_role(roles[page_num*10 + i])
-                    if role is None:
-                        await Selfroles.validate_self_roles(bot, message.channel.guild)
-                        return
-                    add_role = role not in user.roles
-                    try:
-                        await (user.add_roles if add_role else user.remove_roles)(role)
-                    except Forbidden:
-                        if message.channel.permissions_for(message.channel.guild.me).send_messages:
-                            await MessageUtils.send_to(message.channel, "NO", "mute_role_to_high")
-                            return kwargs
-                    else:
-                        if message.channel.permissions_for(message.channel.guild.me).send_messages:
-                            await MessageUtils.send_to(message.channel, "YES", "role_joined" if add_role else "role_left", role_name=await Utils.clean(role.name), delete_after=10)
-                            bot.loop.create_task(remove_reaction(message, reaction, user))
-                            return kwargs
+    if not add:
+        return
+    for i in range(10):
+        if str(reaction) == str(Emoji.get_emoji(str(i+1))):
+            roles = Configuration.get_var(message.guild.id, "SELF_ROLES")
+            role = message.channel.guild.get_role(roles[page_num*10 + i])
+            if role is None:
+                await Selfroles.validate_self_roles(bot, message.channel.guild)
+                return
+            add_role = role not in user.roles
+            try:
+                await (user.add_roles if add_role else user.remove_roles)(role)
+            except Forbidden:
+                if message.channel.permissions_for(message.channel.guild.me).send_messages:
+                    await MessageUtils.send_to(message.channel, "NO", "mute_role_to_high")
+                    return kwargs
+            else:
+                if message.channel.permissions_for(message.channel.guild.me).send_messages:
+                    await MessageUtils.send_to(message.channel, "YES", "role_joined" if add_role else "role_left", role_name=await Utils.clean(role.name), delete_after=10)
+                    bot.loop.create_task(remove_reaction(message, reaction, user))
+                    return kwargs
 
 
     if add:
@@ -147,6 +150,8 @@ async def on_reaction(bot, message_id, channel_id, user_id, reaction):
 
 
 async def remove_reaction(message, reaction, user):
+    if user is None:
+        return
     try:
         await message.remove_reaction(reaction, user)
     except Forbidden:
