e8fa515deaf26ea10479e0ff1e68add219adcb28
AEnterprise <aenterprise2@gmail.com>
2019-04-05 17:24:04 +0200

    add fun cog

--- GearBot/Cogs/ReactionHandler.py
+++ GearBot/Cogs/ReactionHandler.py
@@ -14,11 +14,10 @@ class ReactionHandler(BaseCog):
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        if self.bot.redis_pool is not None:
-            pipe = self.bot.redis_pool.pipeline()
-            pipe.unlink(f"joins:{guild.id}")
-            pipe.unlink(f"inf_track:{guild.id}")
-            await pipe.execute()
+        pipe = self.bot.redis_pool.pipeline()
+        pipe.unlink(f"joins:{guild.id}")
+        pipe.unlink(f"inf_track:{guild.id}")
+        await pipe.execute()
 
 def setup(bot):
     bot.add_cog(ReactionHandler(bot))
