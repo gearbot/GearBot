af5219f1b65916bb9479f557ddf1fc6d60f670d7
AEnterprise <aenterprise2@gmail.com>
2019-04-14 12:51:40 +0200

    should fix crowdin uploading

--- GearBot/Util/Translator.py
+++ GearBot/Util/Translator.py
@@ -176,8 +176,9 @@ async def upload():
     old = hashes.get(target, "")
     if old != new:
         count += 1
+        hashes[target] = new
         message = await tranlator_log('REFRESH', 'Uploading bot translation file')
-        t = threading.Thread(target=upload_files, args=([("lang/en_US.json", "bot/commands.json", {"title": "GearBot bot strings", "export_pattern": "/bot/%locale_with_underscore%.json"}), False]))
+        t = threading.Thread(target=upload_files, args=(("lang/en_US.json", "bot/commands.json", {"title": "GearBot bot strings", "export_pattern": "/bot/%locale_with_underscore%.json"}), False))
         t.start()
         while t.is_alive():
             await asyncio.sleep(1)
