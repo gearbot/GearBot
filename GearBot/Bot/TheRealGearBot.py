7d003e2d9fa226ea243d4df1804b2aebcce5f348
AEnterprise <aenterprise2@gmail.com>
2019-05-04 14:06:08 +0200

    correct timing tracking

--- GearBot/Bot/TheRealGearBot.py
+++ GearBot/Bot/TheRealGearBot.py
@@ -160,7 +160,7 @@ async def on_message(bot, message:Message):
             f = time.perf_counter_ns if hasattr(time, "perf_counter_ns") else time.perf_counter
             start = f()
             await bot.invoke(ctx)
-            bot.metrics.bot_command_timing.labels(command_name=ctx.invoked_with).observe((f() - start) / 1000000)
+            bot.metrics.bot_command_timing.labels(command_name=ctx.command.qualified_name).observe((f() - start) / 1000000)
 
 
 async def on_guild_join(guild):
