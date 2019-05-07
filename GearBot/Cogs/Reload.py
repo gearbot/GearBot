46ddd9a76890d79b92d9c25b8c34fd37d515238f
AEnterprise <aenterprise2@gmail.com>
2019-05-07 21:19:35 +0200

    fix pull

--- GearBot/Cogs/Reload.py
+++ GearBot/Cogs/Reload.py
@@ -114,7 +114,7 @@ class Reload(BaseCog):
         async with ctx.typing():
             code, out, error = await Utils.execute(["git pull origin master"])
         if code is 0:
-            await Pages.create_new(self.bot, "pull", ctx, title=f"{Emoji.get_chat_emoji('YES')} Pull completed with exit code {code}", pages="----NEW PAGE----".join(Pages.paginate(out.decode('utf-8'))))
+            await Pages.create_new(self.bot, "pull", ctx, title=f"{Emoji.get_chat_emoji('YES')} Pull completed with exit code {code}", pages="----NEW PAGE----".join(Pages.paginate(out)))
         else:
             await ctx.send(f"{Emoji.get_chat_emoji('NO')} Pull completed with exit code {code}```yaml\n{out}\n{error}```")
 
