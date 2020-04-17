f5237ee0068454ab332ac5500aa6395574a215eb
AEnterprise <aenterprise2@gmail.com>
2020-04-17 15:07:39 +0200

    gave less errors

--- GearBot/database/DatabaseConnector.py
+++ GearBot/database/DatabaseConnector.py
@@ -18,7 +18,7 @@ class LoggedAttachment(Model):
     id = fields.BigIntField(pk=True, generated=False)
     name = fields.CharField(max_length=100)
     isImage = fields.BooleanField()
-    message = fields.ForeignKeyField("models.LoggedMessage", related_name='attachments', source_field='message')
+    message = fields.ForeignKeyField("models.LoggedMessage", related_name='attachments', source_field='message_id')
 
 
 class CustomCommand(Model):
