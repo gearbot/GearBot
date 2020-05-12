946c427fb8e5771ee075057d1de8ce78047be664
Tarek <13603398+Taarek@users.noreply.github.com>
2020-05-12 14:59:20 +0200

    Censor group invites.

--- GearBot/Cogs/Censor.py
+++ GearBot/Cogs/Censor.py
@@ -61,7 +61,7 @@ class Censor(BaseCog):
                     invite: discord.Invite = await self.bot.fetch_invite(code)
                 except discord.NotFound:
                     await self.censor_invite(ctx, code, "INVALID INVITE")
-                except KeyError:
+                if invite.guild is None:
                     await self.censor_invite(ctx, code, "DM group")
                     censored = True
                 else:
