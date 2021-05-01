e474b07ce83929284bf0fc6b3f898fb12e9ea328
AEnterprise <aenterprise2@gmail.com>
2021-05-01 13:39:33 +0200

    another case of d.py not liking quotes

--- GearBot/Bot/TheRealGearBot.py
+++ GearBot/Bot/TheRealGearBot.py
@@ -17,7 +17,7 @@ from aiohttp import ClientOSError, ServerDisconnectedError
 from discord import Activity, Embed, Colour, Message, TextChannel, Forbidden, ConnectionClosed, Guild, NotFound
 from discord.abc import PrivateChannel
 from discord.ext import commands
-from discord.ext.commands import UnexpectedQuoteError, ExtensionAlreadyLoaded
+from discord.ext.commands import UnexpectedQuoteError, ExtensionAlreadyLoaded, InvalidEndOfQuotedStringError
 
 from Bot import GearBot
 from Util import Configuration, GearbotLogging, Emoji, Pages, Utils, Translator, InfractionUtils, MessageUtils, \
@@ -299,7 +299,7 @@ async def on_command_error(bot, ctx: commands.Context, error):
     elif isinstance(error, Forbidden):
         e = Emoji.get_chat_emoji('BUG')
         await ctx.send(f"{e} Command failed because the discord api responded with \"forbidden\" reply. Please make sure the bot has the permissions and roles required to perform this command {e}")
-    elif isinstance(error, UnexpectedQuoteError):
+    elif isinstance(error, UnexpectedQuoteError) or isinstance(error, InvalidEndOfQuotedStringError):
         e = Emoji.get_chat_emoji('BUG')
         await ctx.send(f"{e} Command parsing failed, unexpected or unclosed quote encountered {e}")
 
